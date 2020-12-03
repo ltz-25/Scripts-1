d0fee92eb9865dda8d9f618e7ecb6b048a3a9403
ltz-25 <73675389+ltz-25@users.noreply.github.com>
2020-12-03 10:14:07 +0800

    Update youth.js

--- Task/youth.js
+++ Task/youth.js
@@ -124,6 +124,10 @@ if ($.isNode()) {
       console.log(`============ 共${cookiesArr.length}个中青账号  =============\n`)
       console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
       console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
+      console.log(`============ CookieYouth${CookieYouth}  =============\n`) 
+      console.log(`============ ARTBODYs${ARTBODYs}  =============\n`) 
+      console.log(`============ REDBODYs${REDBODYs}  =============\n`) 
+      console.log(`============ READTIME${READTIME}  =============\n`)  
     } else {
     cookiesArr.push($.getdata('youthheader_zq'));
     redpArr.push($.getdata('red_zq'));
